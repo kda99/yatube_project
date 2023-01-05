726d0f9a2a1895835e7d634ec7fc7cd30cdf0d60
Дмитрий Капустин <kda99@mail.ru>
2023-01-06 02:52:25 +0300

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse('Главная страница')
 
-def group_posts(request, pk):
+
+def group_posts(request, pk ):
     return HttpResponse(f'Группа номер {pk}')
 
