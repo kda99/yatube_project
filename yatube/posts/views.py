cc5d2195c8dcd4915bddaf3a69a494f7629ed840
Дмитрий Капустин <kda99@mail.ru>
2023-01-06 21:52:44 +0300

    Подключен шаблон главной страницы

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,10 @@
 # Create your views here.
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    return render(request, template)
 
 
 def group_posts(request, pk ):
