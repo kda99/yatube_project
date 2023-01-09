3e020c2c7f4fc360ac830b40e993c367725dfe19
Дмитрий Капустин <kda99@mail.ru>
2023-01-09 23:57:04 +0300

    Спринт 3/11 → Тема 4/7: Шаблоны и теги Django → Урок 4/6

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'post_ns'
+
 urlpatterns = [
-    path('', views.index),
-    path('groups/<slug:pk>/', views.group_posts),
+    path('', views.index, name = 'index'),
+    path('groups/<slug:pk>/', views.group_posts, name = 'post'),
 ]
