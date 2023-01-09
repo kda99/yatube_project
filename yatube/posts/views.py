1393ae096bc6a0e1ebb5a3b369820326cd271cb7
Дмитрий Капустин <kda99@mail.ru>
2023-01-09 16:15:46 +0300

    Теперь текст должен передаваться в шаблон из view-функций через словарь context

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,11 +4,19 @@ from django.shortcuts import render
 
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)
+    text = 'Это главная страница проекта Yatube'
+    context = {
+        'text': text
+    }
+    return render(request, template, context)
 
 
 def group_posts(request, pk ):
     # return HttpResponse(f'Группа номер {pk}')
     template = 'posts/group_list.html'
-    return render(request, template)
+    text = f'Здесь будет информация о группах проекта Yatube номер {pk}'
+    context = {
+        'text': text
+    }
+    return render(request, template, context)
 
