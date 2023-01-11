161c799d461d2b40632ab87d82df65e827e54a34
Дмитрий Капустин <kda99@mail.ru>
2023-01-12 02:51:23 +0300

    Спринт 3/11 → Тема 5/7: База данных и Django ORM → Урок 6/6 - поправил PostAdmin

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -5,11 +5,15 @@ from .models import Group
 
 class PostAdmin(admin.ModelAdmin):
     # Перечисляем поля, которые должны отображаться в админке
-    list_display = ('pk', 'text', 'pub_date', 'author')
+    list_display = ('pk', 'text', 'pub_date', 'author', 'group',)
+    # Это позволит изменять поле group прямо из списка постов
+    list_editable = ('group',)
     # Добавляем интерфейс для поиска по тексту постов
     search_fields = ('text',)
     # Добавляем возможность фильтрации по дате
     list_filter = ('pub_date', )
+    # Замена пустого поля
+    empty_value_display = '-пусто-'
 
 # При регистрации модели Post источником конфигурации для неё назначаем
 # класс PostAdmin
--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -3,6 +3,13 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+class Group(models.Model):
+    title = models.TextField()
+    slug = models.URLField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
 
 class Post(models.Model):
     text = models.TextField()
@@ -12,13 +19,14 @@ class Post(models.Model):
         on_delete=models.CASCADE,
         related_name='posts'
     )
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name='groups'
+    )
 
 
-class Group(models.Model):
-    title = models.TextField()
-    slug = models.URLField()
-    description = models.TextField()
 
-    def __str__(self):
-        return self.title
 
