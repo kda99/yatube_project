c70a341d4c485f153ca366486e42f0c396663354
Дмитрий Капустин <kda99@mail.ru>
2023-01-13 21:16:10 +0300

    Итоговой задание - поправил base.html

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
 
-app_name = 'post_ns'
+app_name = 'posts'
 
 urlpatterns = [
     path('', views.index, name = 'index'),
-    path('groups/<slug:slug>/', views.group_posts, name = 'post'),
+    path('groups/<slug>/', views.group_posts, name = 'group_list'),
 ]
